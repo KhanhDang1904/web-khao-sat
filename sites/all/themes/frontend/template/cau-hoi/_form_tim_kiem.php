86f399606a0f3f650e692eb683e14b2b9a623b02
anhkh <anhkhanh5539574@gmail.com>
2024-01-11 21:31:22 +0700

    Hoan thanh cau hoi

--- sites/all/themes/frontend/template/cau-hoi/_form_tim_kiem.php
+++ sites/all/themes/frontend/template/cau-hoi/_form_tim_kiem.php
@@ -1,17 +1,10 @@
-<?php
-$trangThai = getDanhMucs(26543);
-$thanhToan = getDanhMucs(26626);
 
-?>
-<form id="form-tim-kiem-danh-muc" action="/san-pham">
+<form id="form-tim-kiem-danh-muc" action="/cau-hoi">
   <input class="hidden" name="sort_by" id="sort_bys" value="created">
   <input class="hidden" name="sort_order" id="sort_orders" value="DESC">
   <div class="row breadcrumbs-top">
-    <div class="col-md-4 ">
-      <input placeholder="Mã sản phẩm" name="title" id="title" class="form-control mb-1"value="<?=isset($_GET['title']) ? $_GET['title']:''?>">
-    </div>
-    <div class="col-md-4 ">
-      <input placeholder="Tên sản phẩm" name="field_ten_san_pham_value" id="title" class="form-control mb-1" value="<?=isset($_GET['field_ten_san_pham_value']) ? $_GET['field_ten_san_pham_value']:''?>">
+    <div class="col-md-8">
+      <input placeholder="Tiêu đề" name="title" id="title" class="form-control mb-1"value="<?=isset($_GET['title']) ? $_GET['title']:''?>">
     </div>
     <div class="col-md-2 mb-1">
       <button type="submit" class="btn btn-primary btn-tim-kiem col-12">
@@ -25,7 +18,7 @@ $thanhToan = getDanhMucs(26626);
     <div class="col-md-2">
       <a class="btn btn-outline-primary col-12" href="#" id="btn-them-moi">
         <i class="me-1" data-feather="plus-circle"></i>
-        <span class="align-middle">Thêm mới</span>
+        <span class="align-middle">Thêm</span>
       </a>
     </div>
     <?php endif;?>
