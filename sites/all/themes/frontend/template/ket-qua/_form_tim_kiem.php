0e63ef74deeedf82911a725731ad94391f4006ac
anhkh <anhkhanh5539574@gmail.com>
2024-01-12 17:19:32 +0700

    fix giao dieenj

--- sites/all/themes/frontend/template/ket-qua/_form_tim_kiem.php
+++ sites/all/themes/frontend/template/ket-qua/_form_tim_kiem.php
@@ -1,27 +1,21 @@
-
-<form id="form-tim-kiem-danh-muc" action="/cau-hoi">
+<form id="form-tim-kiem-danh-muc" action="/ket-qua-khao-sat">
   <input class="hidden" name="sort_by" id="sort_bys" value="created">
   <input class="hidden" name="sort_order" id="sort_orders" value="DESC">
   <div class="row breadcrumbs-top">
-    <div class="col-md-8">
-      <input placeholder="Tiêu đề" name="title" id="title" class="form-control mb-1"value="<?=isset($_GET['title']) ? $_GET['title']:''?>">
+    <div class="col-md-2">
+      <input placeholder="Khảo sát" name="title" id="title" class="form-control mb-1" value="<?=isset($_GET['title']) ? $_GET['title']:''?>">
+    </div>
+    <div class="col-md-3">
+      <input placeholder="Họ tên" name="field_ho_ten_value"  class="form-control mb-1" value="<?=isset($_GET['field_ho_ten_value']) ? $_GET['field_ho_ten_value']:''?>">
+    </div>
+    <div class="col-md-3">
+      <input placeholder="Gmail" name="mail"  class="form-control mb-1" value="<?=isset($_GET['mail']) ? $_GET['mail']:''?>">
     </div>
     <div class="col-md-2 mb-1">
       <button type="submit" class="btn btn-primary btn-tim-kiem col-12">
         <i data-feather='search'></i> Tìm kiếm
       </button>
     </div>
-    <?php
-    global $user;
-    if (!isset($user->roles[8])):
-    ?>
-    <div class="col-md-2">
-      <a class="btn btn-outline-primary col-12" href="#" id="btn-them-moi">
-        <i class="me-1" data-feather="plus-circle"></i>
-        <span class="align-middle">Thêm</span>
-      </a>
-    </div>
-    <?php endif;?>
   </div>
 
 </form>
